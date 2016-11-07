fcb993a203e815fefef874c71a09699347644ac1
mrpp01 <mrpp01@gmail.com>
2016-11-07 15:29:31 -0500

    Initial commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '10.0'
+use_frameworks!
+
+# The target name is most likely the name of your project.
+target 'Schedule' do
+
+    pod 'React', :path => './node_modules/react-native', :subspecs => [
+        'Core',
+        'RCTText',
+        'RCTNetwork',
+        'RCTWebSocket',
+        'RCTImage',
+]
+
+end
--- Schedule/ViewController.swift
+++ Schedule/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
+    var rootView: RCTRootView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let jsCodeLocation = NSURL(string: "http://localhost:8081/index.ios.bundle?platform=ios") as URL!
+        
+        self.rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "Schedule",
+            initialProperties:nil,
+            launchOptions: nil
+            
+        )
+        
+        let view  = (
+            x: self.view.bounds.origin.x,
+            y: self.view.bounds.origin.y,
+            width: self.view.bounds.width,
+            height: self.view.bounds.height
+        )
+        
+        rootView.frame = CGRect(x: view.x, y: view.y, width: view.width, height: view.height)
+        
+        
+        self.view.addSubview(rootView)
+    
     }
 
     override func didReceiveMemoryWarning() {
